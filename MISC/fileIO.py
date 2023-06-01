b86e6c2157c3aec73318e227bed878f68b0736b5
Sylwia Lewera <Sylwia_Lewera@epam.com>
2023-06-01 16:57:20 +0200

    section 1 exercises

---
+++ MISC/fileIO.py
@@ -0,0 +1,12 @@
+file = open('inputFile.txt', 'r')
+passFile = open('passFile.txt', 'w')
+failFile = open('failFile.txt','w')
+for line in file:
+    line_split = line.split()
+    if line_split[2] == 'P':
+        passFile.write(line)
+    else:
+        failFile.write(line)
+file.close()
+passFile.close()
+failFile.close()
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PythonTAIntro
 Using Python for Automation - LinkedIn Training
+
+Training Agenda:
+
+1. Automate File, Folder, and Terminal Interactions
+
+2. Web Scraping with Beautiful Soup
+
+3. Automate Web Browsing with Selenium
+
+4. Automating with APIs
